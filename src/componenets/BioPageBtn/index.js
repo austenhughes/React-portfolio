083e79db43425b1a7cd151d0155b992d6c989698
Austn Hughes <austen.hughes.91@gmail.com>
2021-06-01 21:59:54 -0600

    crossed wires part 2

--- src/componenets/BioPageBtn/index.js
+++ src/componenets/BioPageBtn/index.js
@@ -4,7 +4,7 @@ import { Button } from '@material-ui/core';
 
 function BioPageBtn(props) {
   return (
-    <Button className="btn" href="/bio" target="_"><input 
+    <Button className="btn" href="/bio-React" target="_"><input 
     type="button" value="About Me" className="btn"/></Button>
   );
 }
--- src/componenets/HomePageBtn/index.js
+++ src/componenets/HomePageBtn/index.js
@@ -4,7 +4,7 @@ import { Button } from '@material-ui/core';
 
 function HomePageBtn(props) {
   return (
-    <Button className="btn" href="/home" target="_"><input 
+    <Button className="btn" href="/React-portfolio" target="_"><input 
     type="button" value="Home" className="btn"/></Button>
   );
 }
